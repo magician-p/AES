9ed354d0d0c57bea8d3cf70bf85ae4a81e34f32b
MagicianP <wh983144765@gmail.com>
2026-05-01 20:06:35 +0800

    首次提交

---
+++ AES.h
@@ -0,0 +1,9 @@
+//
+// Created by 98314 on 2026/5/1.
+//
+
+#ifndef AES_H
+#define AES_H
+    void AES(char *plaintext,unsigned char *key);
+    void De_AES(char *ciphertext,unsigned char *key);
+#endif //AES_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// TIP 要<b>Run</b>代码，请按 <shortcut actionId="Run"/> 或点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+int main() {
+
+    return 0;
+}
